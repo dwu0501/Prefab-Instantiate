80a37113f5790c2066813b177581823033ed2557
dwu0501 <101563675+dwu0501@users.noreply.github.com>
2022-05-06 14:32:09 -0300

    Prefab & Instantiate (Incomplete)

---
+++ Assets/ButtonBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    void Boton()
+   {
+       Debug.Log("Hello World");
+   }
+}
---
+++ Assets/Instantitator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantitator : MonoBehaviour
+{
+    public GameObject objectToClone;
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+        int counter = 0;
+        while (counter<3){
+            
+        Instantiate(objectToClone);
+            counter++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TripleInstantiate()
+    {
+        int counter = 0;
+        while (counter < 3)
+        {
+
+            Instantiate(objectToClone);
+            counter++;
+        }
+    }
+}
